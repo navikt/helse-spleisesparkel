ca1f0243e7b868db9f3a2d50929dca022b8c309b
Linus Johansson <linus.123@hotmail.com>
2026-02-04 15:25:08 +0100

    Nye versjoner av rapdsAndRivers og tbdLibs
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 val junitJupiterVersion = "6.0.1"
-val rapidsAndRiversVersion = "2026011411051768385145.e8ebad1177b4"
-val tbdLibsVersion = "2026.01.22-09.16-1d3f6039"
+val rapidsAndRiversVersion = "2026012807431769582626.d6f80c5a169d"
+val tbdLibsVersion = "2026.01.28-07.21-5436e475"
 val ktorVersion = "3.2.3"
 val mockkVersion = "1.13.17"
 val wiremockVersion = "3.9.2"
