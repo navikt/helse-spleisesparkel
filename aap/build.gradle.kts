195632e86120b33e2b15dbc0fdd8000612fcfe76
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-03-18 10:42:31 +0100

    Legger på et mellomrom i en byggefil, for å verifisere at dette bare trigger bygg i aap-modulen

--- aap/build.gradle.kts
+++ aap/build.gradle.kts
@@ -1,5 +1,6 @@
 val ktorVersion: String by project
 val tbdLibsVersion: String by project
+
 dependencies {
     implementation("com.github.navikt.tbd-libs:azure-token-client-default:$tbdLibsVersion")
     implementation("com.github.navikt.tbd-libs:retry:$tbdLibsVersion")
